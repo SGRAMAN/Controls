565e84fae24bb91e063bec476a2611048f42ec25
SGRAMAN <sgraman.subramanian@gmail.com>
2021-12-03 00:30:19 +0530

    commit to remote

---
+++ src/org/pract/Login.java
@@ -0,0 +1,16 @@
+package org.pract;
+
+public class Login {
+public static void main(String[] args) {
+	int evennumber = 10;
+	int oddnumber = 7;
+	if (evennumber==10) {
+		System.out.println("10 is a even  number");
+	}
+	
+	
+	 else if(oddnumber==6) {
+		 System.out.println("7 is a numbers");
+	 }
+}
+}
